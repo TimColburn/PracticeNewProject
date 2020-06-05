c1e5a57109186c85813b1892da33d9159e43046f
TimColburn <tim_colburn@yahoo.com>
2020-06-05 12:21:09 -0700

    Selenium test works

---
+++ PracticeNewProject.UITests/AppShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PracticeNewProject.UITests
+{
+    public class AppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44315/");
+            }
+        }
+    }
+}
