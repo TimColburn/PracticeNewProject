e391d8376919f37c83afcb883f2ccbd67528da33
TimColburn <tim_colburn@yahoo.com>
2020-06-09 10:02:51 -0700

    Small update to Notes.txt

--- PracticeNewProject/Data/NewProjectDBInitializer.cs
+++ PracticeNewProject/Data/NewProjectDBInitializer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 
-namespace PracticeNewProject.Data
+namespace PracticeNewProject.Data 
 {
     public class NewProjectDBInitializer : DropCreateDatabaseAlways<NewProjectContext>
     {
